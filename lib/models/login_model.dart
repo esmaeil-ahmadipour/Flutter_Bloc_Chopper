2256e7824b07718cec577143f7cbe4e76d726320
Esmaeil Ahmadipour <software8686@gmail.com>
2020-04-29 14:37:42 +0430

    commit> Create Login Model part file NOTE: for create this file , write in terminal : flutter pub run build_runner build

--- lib/models/login_model.dart
+++ lib/models/login_model.dart
@@ -1,8 +1,13 @@
 
 import 'package:json_annotation/json_annotation.dart';
 
+// Add This Line Before build_runner
+part 'login_model.g.dart';
+
 @JsonSerializable(nullable: false)
 class Login{
-String token; //"token": "QpwL5tke4Pnpja7X4"
+String token;
+// Add This Line Before build_runner
+Login(this.token); //"token": "QpwL5tke4Pnpja7X4"
 
 }
