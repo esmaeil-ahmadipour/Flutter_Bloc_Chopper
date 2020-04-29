c16040a2de7813c2ac98d994cdba6087114d9774
Esmaeil Ahmadipour <software8686@gmail.com>
2020-04-29 14:57:14 +0430

    commit> Add Method For Login Model / Create ApiService part file (by build_runner)

--- lib/models/login_model.dart
+++ lib/models/login_model.dart
@@ -10,4 +10,7 @@ String token;
 // Add This Line Before build_runner
 Login(this.token); //"token": "QpwL5tke4Pnpja7X4"
 
+factory Login.fromJson(Map<String , dynamic> json) => _$LoginFromJson(json);
+
+  Map<String , dynamic> toJson() => _$LoginToJson(this);
 }
--- lib/services/api_services.dart
+++ lib/services/api_services.dart
@@ -1,9 +1,9 @@
 import 'package:chopper/chopper.dart';
 
+part 'api_services.chopper.dart';
+
 @ChopperApi(baseUrl: "")
-abstract class ApiService extends ChopperService{
-@Post()
-  Future<Response> getResult(@Body() Map<String , dynamic> body);
-
-
+abstract class ApiService extends ChopperService {
+  @Post()
+  Future<Response> getResult(@Body() Map<String, dynamic> body);
 }
